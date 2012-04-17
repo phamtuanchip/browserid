ae3893c2e2c29a96e9457aaf0a6f9d27eb4b2afa
Shane Tomlinson <stomlinson@mozilla.com>
2012-04-17 16:57:50 +0100

    Complete the signup page conversion.

--- resources/static/pages/start.js
+++ resources/static/pages/start.js
@@ -45,7 +45,8 @@ $(function() {
     module.start({});
   }
   else if (path === "/signup") {
-    bid.signUp();
+    var module = bid.signUp.create();
+    module.start({});
   }
   else if (path === "/forgot") {
     bid.forgot();
